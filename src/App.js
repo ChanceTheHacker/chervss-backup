713b1723f5432185ce866fa7a6ef1e419df6eeda
wcperagine <ertyjunior@gmail.com>
2018-08-24 13:30:05 +0000

    here goes nothing

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link, // eslint-disable-line
+  Switch
+} from 'react-router-dom';
+
+import ImageBackground from './components/background/ImageBackground';
+import HeaderComponent from './components/header/HeaderComponent';
+import HomeComponent from './components/main/HomeComponent';
+import WishListComponent from './components/main/WishListComponent';
+import RegisterComponent from './components/main/RegisterComponent';
+import NotFoundComponent from './components/main/NotFoundComponent';
+import FooterComponent from './components/footer/FooterComponent';
+
+import './app.css';
+
+
+class App extends Component {
+  render() {
+    return (
+
+        <Router>
+          <div>
+            <ImageBackground/>
+            
+
+            <HeaderComponent/>
+
+            <Switch>
+              <Route exact path='/' component={HomeComponent}/>
+              <Route exact path='/wishlist' component={WishListComponent}/>
+              <Route exact path='/register' component={RegisterComponent}/>
+              <Route component={NotFoundComponent}/>
+            </Switch>
+
+            <FooterComponent/>
+          </div>
+        </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/background/ImageBackground.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './background.css';
+
+function ImageBackground (){
+ return (
+ 	<img id="background" src="christmas-background.jpg" alt="great photo">
+ 	</img>
+ )
+}
+
+export default ImageBackground;
